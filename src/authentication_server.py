9308c98d5f477b33895248d795b597fe6447182e
Lucas Guimarães <lgf@cin.ufpe.br>
2025-03-09 21:37:40 -0300

    solve error with dns requests

--- src/authentication_server.py
+++ src/authentication_server.py
@@ -1,19 +1,29 @@
 from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread
+from threading import Thread, Lock
 from utils import readjson, writejson
 
+public_keys_mutex = Lock()
+
 def handle_request(socket_client):
+    global public_keys_mutex
     login = socket_client.recv(2048).decode()
+    if (login == ''): # DNS check
+        socket_client.close()
+        return
+    
     socket_client.send("ok".encode())
 
     public_key = socket_client.recv(2048).decode()
+    
+    with public_keys_mutex:
+        keys = readjson("public_keys") or dict()
+        keys[login] = [public_key, False]
+        writejson(keys, "public_keys")
+        keys = readjson("public_keys")
+    
+    assert(keys[login] == [public_key, False])
     socket_client.send("ok".encode())
-    
     socket_client.close()
-    
-    keys = readjson("public_keys") or dict()
-    keys[login] = [public_key, False]
-    writejson(keys, "public_keys")
 
 
 def auth_server():
@@ -24,7 +34,7 @@ def auth_server():
 
     while 1:
         socket_client, addr_client = server_socket.accept()
-        print(f"Established connection with {addr_client}")
+        # print(f"Established connection with {addr_client}")
         Thread(target=handle_request, args=(socket_client,)).start()
 
 if __name__ == '__main__':
